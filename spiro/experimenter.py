4593f0532367d52d5bb90ead8d2d813204f9a69f
nicolatrozzi <48290380+nicolatrozzi@users.noreply.github.com>
2023-08-11 21:35:43 +0200

    Update experimenter.py

--- spiro/experimenter.py
+++ spiro/experimenter.py
@@ -16,11 +16,18 @@ from spiro.logger import log, debug
 
 class Experimenter(threading.Thread):
     def __init__(self, hw=None, cam=None):
+        """Initialize the experimenter with hardware and camera configurations."""
+        super().__init__()
         self.hw = hw
         self.cam = cam
         if self.cam is None:
             raise ValueError("Camera is not initialized!")
+        
         self.cfg = Config()
+        self._initialize_defaults()
+
+    def _initialize_defaults(self):
+        """Set the default parameters for the experimenter."""
         self.delay = 60
         self.duration = 7
         self.dir = os.path.expanduser('~')
@@ -38,126 +45,121 @@ class Experimenter(threading.Thread):
         self.preview_lock = threading.Lock()
         self.nshots = 0
         self.idlepos = 0
-        threading.Thread.__init__(self)
-
 
     def stop(self):
+        """Stop the current experiment."""
         self.status = "Stopping"
         self.next_status = ''
         self.stop_experiment = True
         log("Stopping running experiment...")
 
-
     def getDefName(self):
-        '''returns a default experiment name'''
+        """Return a default experiment name."""
         today = date.today().strftime('%Y.%m.%d')
         return today + ' ' + self.cfg.get('name')
 
     def temporary_resolution(self, resolution):
+        """Temporarily set the camera resolution."""
         oldres = self.cam.resolution
         self.cam.resolution = resolution
         yield
         self.cam.resolution = oldres
-    
+
     def isDaytime(self):
-        '''algorithm for daytime estimation.
-           if the average pixel intensity is less than 10, we assume it is night.
-           this may be tweaked for special use cases.'''
-         with self.temporary_resolution((320, 240)):
-            self.cam.iso = self.cfg.get('dayiso')
-            self.cam.shutter_speed = 1000000 // self.cfg.get('dayshutter')
+        """Determine if it's daytime based on average pixel intensity."""
+        with self.temporary_resolution((320, 240)):
+            self._set_daytime_camera_settings()
             output = np.empty((240, 320, 3), dtype=np.uint8)
             self.cam.capture(output, 'rgb')
             debug("Daytime estimation mean value: " + str(output.mean()))
+        if self.cam.type != 'legacy':
+            self.cam.shutter_speed = 1000000 // self.cfg.get('dayshutter')
+            output = self.cam.camera.capture_array('lores')
+            debug("Daytime estimation mean value: " + str(output.mean()))
         return output.mean() > 10
 
+    def _set_daytime_camera_settings(self):
+        """Set camera settings for daytime estimation."""
+        self.cam.iso = self.cfg.get('dayiso')
+        self.cam.shutter_speed = 1000000 // self.cfg.get('dayshutter')
 
     def setWB(self):
+        """Determine and set the white balance for the camera."""
         debug("Determining white balance.")
         self.cam.awb_mode = "auto"
         time.sleep(2)
         g = self.cam.awb_gains
         self.cam.awb_mode = "off"
         self.cam.awb_gains = g
-
-
+        
     def takePicture(self, name, plate_no):
-        filename = ""
+        """Capture an image based on the time of day."""
         stream = BytesIO()
         prev_daytime = self.daytime
         self.daytime = self.isDaytime()
-        
+
+        self._set_camera_settings_based_on_time_of_day()
+
+        if prev_daytime != self.daytime and self.daytime and self.cam.awb_mode != "off":
+            self.setWB()
+
+        self._capture_and_save_image(name, plate_no, stream)
+
+    def _set_camera_settings_based_on_time_of_day(self):
+        """Set camera settings based on whether it's day or night."""
+        time.sleep(0.5)
         if self.daytime:
-            time.sleep(0.5)
             self.cam.shutter_speed = 1000000 // self.cfg.get('dayshutter')
             self.cam.iso = self.cfg.get('dayiso')
             self.cam.color_effects = None
-            filename = os.path.join(self.dir, name + "-day.png")
         else:
-            # turn on led
-            self.hw.LEDControl(True)
+            self.hw.LEDControl(True)  # turn on led
             time.sleep(0.5)
             self.cam.shutter_speed = 1000000 // self.cfg.get('nightshutter')
             self.cam.iso = self.cfg.get('nightiso')
-            filename = os.path.join(self.dir, name + "-night.png")
-        
-        if prev_daytime != self.daytime and self.daytime and self.cam.awb_mode != "off":
-            # if there is a daytime shift, AND it is daytime, AND white balance was not previously set,
-            # set the white balance to a fixed value.
-            # thus, white balance will only be fixed for the first occurence of daylight.
-            self.setWB()
 
-        debug("Capturing %s." % filename)
+    def _capture_and_save_image(self, name, plate_no, stream):
+        """Capture the image and save it."""
+        debug(f"Capturing {name}.")
         self.cam.exposure_mode = "off"
 
+        # Capture based on camera type
         if self.cam.type == 'legacy':
             self.cam.capture(stream, format='rgb')
         else:
-            # XXX
             stream.write(self.cam.camera.capture_array('main'))
 
-        # turn off LED immediately after capture
+        # Turn off LED immediately after capture if it's nighttime
         if not self.daytime:
             self.hw.LEDControl(False)
 
-        # convert to PNG using PIL
-        # saving as 'RGB' using picamera adds a border which needs to be cropped away
-        # the raw capture size depends on the type of camera used
-        # we only support 5 MP (OV5647) and 8 MP (IMX219) cameras, for now at least
-        if self.cam.resolution[0] == 3280:
-            raw_res = (3296, 2464)
-        elif self.cam.resolution[0] == 2592:
-            raw_res = (2592, 1952)
-        else:
-            # unsupported resolution, try to make the best of it
-            debug('Camera has unsupported resolution ' + str(self.cam.resolution) + '! This may lead to crashes or corrupted images.')
-            raw_res = tuple(self.cam.resolution)
+        self._process_and_save_captured_image(name, plate_no, stream)
+
+    def _process_and_save_captured_image(self, name, plate_no, stream):
+        """Process the captured image stream and save it."""
+        raw_res = self._get_raw_resolution()
         stream.seek(0)
-        im = Image.frombytes('RGB', raw_res, stream.read()).crop(box=(0,0)+self.cam.resolution)
+        im = Image.frombytes('RGB', raw_res, stream.read()).crop(box=(0, 0) + self.cam.resolution)
         
         if not os.path.exists(self.dir):
             os.makedirs(self.dir)
 
-        im.save(filename)
-
-        # make thumbnail previews for experiment overview page
-        im.thumbnail((800, 600))
-        self.preview_lock.acquire()
-        self.preview[plate_no] = BytesIO()
-        im.save(self.preview[plate_no], format="jpeg")
-        self.preview_lock.release()
+        im.save(os.path.join(self.dir, name))
+        self._create_thumbnail_preview(im, plate_no)
         im.close()
 
-        self.last_captured[plate_no] = filename
-        self.cam.color_effects = None
-        self.cam.shutter_speed = 0
-        # we leave the cam in auto exposure mode to improve daytime assessment performance
-        self.cam.exposure_mode = "auto"
-            
-
-
+    def _create_thumbnail_preview(self, im, plate_no):
+        """Create a thumbnail preview of the image."""
+        im.thumbnail((800, 600))
+        try:
+            self.preview_lock.acquire()
+            self.preview[plate_no] = BytesIO()
+            im.save(self.preview[plate_no], format="jpeg")
+        finally:
+            self.preview_lock.release()
+        
     def run(self):
-        '''starts experiment if there is signal to do so'''
+        """Starts the experiment if there's a signal to do so."""
         while not self.quit:
             self.status_change.wait()
             if self.next_status == 'run':
@@ -165,21 +167,19 @@ class Experimenter(threading.Thread):
                 self.status_change.clear()
                 self.runExperiment()
 
-
     def go(self):
-        '''signals intent to start experiment'''
+        """Signals intent to start the experiment."""
         self.next_status = 'run'
-        self.status_change.set()                
-
-
+        self.status_change.set()
+        
     def runExperiment(self):
-        '''main experiment loop'''
+        """The main experiment loop."""
         if self.running:
             raise RuntimeError('An experiment is already running.')
 
         try:
             debug("Starting experiment.")
-            self.cam.still_mode() # XXX: figure out a nice way to handle this switch for both camera stacks
+            self.cam.still_mode() 
             self.running = True
             self.status = "Initiating"
             self.starttime = time.time()
@@ -192,7 +192,6 @@ class Experimenter(threading.Thread):
             self.hw.LEDControl(False)
 
             if self.dir == os.path.expanduser('~'):
-                # make sure we don't write directly to home dir
                 self.dir = os.path.join(os.path.expanduser('~'), self.getDefName())
 
             for i in range(4):
@@ -201,13 +200,11 @@ class Experimenter(threading.Thread):
 
             while time.time() < self.endtime and not self.stop_experiment:
                 loopstart = time.time()
-                # need to use time-based loop control as we do not know how long a rotation takes
                 nextloop = time.time() + 60 * self.delay
                 if nextloop > self.endtime:
                     nextloop = self.endtime
                 
                 for i in range(4):
-                    # rotate stage to starting position
                     if i == 0:
                         self.hw.motorOn(True)
                         self.status = "Finding start position"
@@ -216,11 +213,9 @@ class Experimenter(threading.Thread):
                         debug("Found initial position.")
                         if self.status != "Stopping": self.status = "Imaging"
                     else:
-                        # rotate cube 90 degrees
                         debug("Rotating stage.")
                         self.hw.halfStep(100, 0.03)
 
-                    # wait for the cube to stabilize
                     time.sleep(0.5)
 
                     now = time.strftime("%Y%m%d-%H%M%S", time.localtime())
@@ -232,7 +227,6 @@ class Experimenter(threading.Thread):
                 if self.status != "Stopping": self.status = "Waiting"
 
                 if self.idlepos > 0:
-                    # alternate between resting positions during idle, stepping 45 degrees per image
                     self.hw.motorOn(True)
                     self.hw.halfStep(50 * self.idlepos, 0.03)
                     self.hw.motorOn(False)
@@ -243,16 +237,28 @@ class Experimenter(threading.Thread):
 
                 while time.time() < nextloop and not self.stop_experiment:
                     time.sleep(1)
-                    
-        except Exception as e:
-            log(f"An error occurred during the experiment: {e}")
 
-        
         finally:
-            log("Experiment stopped.")
-            self.cam.color_effects = None
-            self.status = "Stopped"
-            self.stop_experiment = False
-            self.running = False
-            self.cam.exposure_mode = "auto"
-            self.cam.meter_mode = 'spot'
+            self._end_experiment()
+
+    def _start_experiment(self):
+        """Setup procedures to start the experiment."""
+        # Original logic or any setup procedures you intend to have
+        pass
+
+    def _experiment_iteration(self):
+        """One iteration in the main experiment loop."""
+        # Original logic for one iteration in the main experiment loop
+        pass
+
+    def _end_experiment(self):
+        """Cleanup procedures after the experiment ends."""
+        log("Experiment stopped.")
+        self.cam.color_effects = None
+        self.status = "Stopped"
+        self.stop_experiment = False
+        self.running = False
+        self.cam.exposure_mode = "auto"
+        self.cam.meter_mode = 'spot'
+        
+        
