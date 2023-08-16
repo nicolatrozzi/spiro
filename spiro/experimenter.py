a47fff2f1cd9cddb919829e8e11122626eddc199
nicolatrozzi <48290380+nicolatrozzi@users.noreply.github.com>
2023-08-16 17:04:33 +0200

    Update experimenter.py

--- spiro/experimenter.py
+++ spiro/experimenter.py
@@ -88,7 +88,7 @@ class Experimenter(threading.Thread):
             self.hw.LEDControl(True)
             time.sleep(0.5)
             self.cam.shutter_speed = 1000000 // 5
-            self.cam.iso = 3000
+            self.cam.iso = 2000
             filename = os.path.join(self.dir, name + "-night.png")
         
         if prev_daytime != self.daytime and self.daytime and self.cam.awb_mode != "off":
